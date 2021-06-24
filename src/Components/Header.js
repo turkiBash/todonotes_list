dd7a530311bf6e89b952fa9501061c56ae12adcf
turkibash <dev@turki.tech>
2021-06-24 17:03:16 +0300

    initial

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+
+
+
+const Header = () => {
+
+    return (
+        <div className="Header">
+            <h1>
+                hiiiiiiii
+            </h1>
+        </div>
+    )
+}
+
+export default Header
